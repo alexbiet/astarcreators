91f9e795afc01517a98ec3bd0ce6ae6df14ceade
Alex Biet <alexandrubiet@gmail.com>
2022-06-10 02:20:59 +0200

    node server wip

---
+++ server.js
@@ -0,0 +1,40 @@
+// -----------------------
+// MINT NFT             //
+// -----------------------
+
+
+const ApiPromise = require('@polkadot/api/promise');
+// const WsProvider = require('@polkadot/api');
+// var typesBundleForPolkadot = require('@crustio/type-definitions');
+
+
+console.log(ApiPromise);
+// console.log(WsProvider);
+
+
+
+// import {ApiPromise, WsProvider} from '@polkadot/api';
+// import {typesBundleForPolkadot} from '@crustio/type-definitions';
+// import { create } from 'ipfs-http-client';
+
+// async function main() {
+//   const api = new ApiPromise({
+//     provider: new WsProvider('wss://api.crust.network'),
+//     typesBundle: typesBundleForPolkadot,
+//   });
+//   await api.isReady;
+//   console.log(123);
+//   // Use api
+// }
+
+// main();
+
+
+// var unique = require('uniq');
+
+// var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+// console.log(unique(data));
+
+
+
