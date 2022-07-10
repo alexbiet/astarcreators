4d7f340c38b65a7a2d23e673f3d1feee47730ce4
Markeljan <Markeljan19@gmail.com>
2022-07-10 14:05:39 -0400

    deposit trests

--- static/main.js
+++ static/main.js
@@ -2058,6 +2058,19 @@ const postNFT = async () => {
   }
 };
 
+
+//////////////////////////////
+///REAL dApp Staking !!///
+///////////////////////////////
+// function bond_and_stake(address, uint128) external;
+
+ const DAPPS_WRITE = new ethers.Contract(addresses[chain].dAppsStaking, abis.dAppsStaking, signer);
+ const DAPPS_READ = new ethers.Contract(addresses[chain].dAppsStaking, abis.dAppsStaking, provider);
+
+//await DAPPS_WRITE.bond_and_stake("0xE9CedB215bf0b509140EA4c9D1175Fc78c1A6aF8", 1);
+
+//console.log(await DAPPS_READ.read_current_era());
+
 ////////////////////
 ///ADAO STAKING ///
 ///////////////////
@@ -2073,7 +2086,7 @@ const postNFT = async () => {
 
 
 // async function deposit(_address, _amount) {
-//   ADAO_WRITE.depositFor(_address, {value: ethers.utils.parseEther(_amount.toString()) });
+// ADAO_WRITE.depositFor(_address, {value: ethers.utils.parseEther(_amount.toString()) });
   
 
 //   console.log("deposited");
